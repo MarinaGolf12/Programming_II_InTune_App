f732821f765555b99a0b55eb7861cc0e57a43eed
fouktouk <116193315+fouktouk@users.noreply.github.com>
2023-01-04 21:16:53 +0200

    Add files via upload

---
+++ EntranceTest.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import org.junit.Before;
+
+class EntranceTest {
+
+	Entrance entrance;
+
+	@Before
+	void setUp() throws Exception {
+
+		entrance = new Entrance();
+	}
+
+	@Test
+	public void testEntrance() {
+
+		entrance.Entrance();
+	}
+
+
+
+
+}
