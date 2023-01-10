7a6f1c70d4df31b0f1757a8c2bf78ba8e086a990
orestis <63724084+firgolis@users.noreply.github.com>
2023-01-10 19:57:14 +0200

    Add files via upload

---
+++ ChatTest.java
@@ -0,0 +1,40 @@
+//import required classes and packages
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import org.junit.*;
+
+
+class ChatTest {
+    
+    private Chat chat;
+
+    private String a = "sample";
+
+    private JButton send = new JButton("send");
+
+    private JButton back = new JButton("back");
+
+    private JButton test = new JButton("back");
+
+    private Messages msg;
+
+    @Before
+		public void setUp()  {
+
+			chat = new Chat(a);
+
+		}
+
+		public void testChat () {
+            if (test != back) {
+                if (test == send) { //user to send msg
+
+                    Chat chat = new Chat(a);
+                } 
+            } else {
+                msg = new Messages();
+            }
+        }
+}
