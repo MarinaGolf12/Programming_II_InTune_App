594ca2f1b09f1638e4d20785dc481e96474be81a
orestis <63724084+firgolis@users.noreply.github.com>
2023-01-11 00:31:37 +0200

    Add files via upload

---
+++ HomepageTest.java
@@ -0,0 +1,40 @@
+//import required classes and packages
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import org.junit.*;
+
+public class HomepageTest {
+    
+    private Homepage homepage;
+    private JButton postbutton = new JButton("New Post");
+    private JButton newpostbutton = new JButton("Watch posts");
+    private JButton chatbutton = new JButton("Chat");
+    private JButton profilebutton = new JButton("Profile");
+    private JButton logoffbutton = new JButton("Logoff");
+    private JButton test = new JButton("test");
+    private Post post;
+    private WatchPost wp;
+    private Messages msg;
+    private Profile profile;
+    private Logoff logoff;
+
+    @Before
+		public void setUp()  {
+			homepage = new Homepage();
+		}
+
+		public void testHomepage () {
+            if (test ==  postbutton) {
+                post = new Post();
+            } else if (test == newpostbutton) {
+                wp = new WatchPost();
+            } else if (test == chatbutton) {
+                msg = new Messages();
+            } else if (test == profilebutton) {
+                profile = new Profile();
+            } else if (test == logoffbutton) {
+                logoff = new Logoff();
+            }
+        }
+}
